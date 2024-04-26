f49a08baee90751618721742ba84adcf6cbd094a
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-04-26 18:30:16 -0400

    Update basePath in next.config.js to "/awesome-social-agents"

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "",
+    basePath: "/awesome-social-agents",
   
     /**
      * Disable server-based image optimization. Next.js does not support
