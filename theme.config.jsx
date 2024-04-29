0c72431b9c6684b246bca130b74a3071331783cc
Hao <prokilchu@gmail.com>
2024-04-29 11:24:43 -0400

    chore: Update npm dependencies and add uvcanvas package

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,12 @@ export default {
     logo: <span>Awesome Social Agents</span>,
     project: {
       link: 'https://github.com/sotopia-lab/awesome-social-agents',
+    },
+    toc: {
+      backToTop: true,
+    },
+    sidebar: {
+      toggleButton: true,
     }
     // ... other theme options
   }
